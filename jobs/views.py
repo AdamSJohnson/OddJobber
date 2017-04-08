0adff14fc9b2065ebfa6f2772e93ac02708e2179
Jarred Durant <durantjm198@potsdam.edu>
2017-04-08 18:13:05 -0400

    Create job page

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponseRedirect
+from . import models
 
 def createJob(request):
+    if request.method == "POST":
+        title = request.POST.get("jobtitle")
+        description = request.POST.get("description")
+        price = request.POST.get("price")
+        zipcode = request.POST.get('zipcode')
+        duedate = request.POST.get('duedate')
+        newjob = models.Job(title=title, description=description,
+                            price=price, zipcode=zipcode, due_date=duedate)
+        newjob.save()
+        return HttpResponseRedirect('/jobs/createJob')
     return render(request, 'createJob.html')
