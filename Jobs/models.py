c0265bf531c5a8f49fd82f27759b636c0a0aeea9
Jarred Durant <durantjm198@potsdam.edu>
2017-04-08 16:21:36 -0400

    Spicy job model added

--- Jobs/models.py
+++ Jobs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Job(models.Model):
+    title = models.CharField(default='')
+    description = models.TextField(default='')
+    zipcode = models.CharField(default='')
+    price = models.DecimalField(max_digits=2)
+    # tags: TODO
+    due_date = models.DateField()
+    # creator: user TODO
