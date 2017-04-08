6d83ac22d6ef9349ea510eaefcf720393a7e4d1a
GarryG <valepos@gmail.com>
2017-04-08 16:35:31 -0400

    refactored Jobs to jobs

--- OddJobber/urls.py
+++ OddJobber/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^Jobs/', include('Jobs.urls')),
+    url(r'^jobs/', include('jobs.urls')),
     url(r'^home/', include('OddJobber.home.urls'))
 ]
