db89e6e0e2073b118f9975f07e66d9565a96a33d
GarryG <valepos@gmail.com>
2017-04-08 19:01:18 -0400

    Fixing up log in

--- home/forms.py
+++ home/forms.py
@@ -1,13 +1,9 @@
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from . models import UserProfile
+from django import forms
 
-class UserForm(ModelForm):
+class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = '__all__'
-
-class UserProfileForm(ModelForm):
-    class Meta:
-        model = UserProfile
-        exclude = ['user']
+        fields = ['email', ]
--- home/views.py
+++ home/views.py
@@ -3,22 +3,38 @@ from . forms import UserForm, UserProfileForm
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 
-def signup(request):
-    if request.method == 'POST':
-        uf = UserForm(request.POST, prefix = 'user')
-        upf = UserProfileForm(request.POST, prefix = 'userprofile')
-        if uf.is_valid() * upf.is_valid():
-            user = uf.save()
-            userprofile = upf.save(commit = False)
-            userprofile.user = user
-            userprofile.save()
-            return HttpResponseRedirect('/home/')
+from django.contrib.auth.forms import PasswordResetForm
+from django.shortcuts import redirect
+from django.views.generic import CreateView
 
-    else:
-        uf = UserForm(prefix='user')
-        upf = UserProfileForm(prefix = 'userprofile')
-    return render_to_response('signup.html',
-                               dict(userform = uf, userprofileform = upf))
+from . forms import UserForm
+
+from . models import UserProfile
+
+class signup(CreateView):
+    form_class = UserForm
+    model = UserProfile
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.set_password(User.objects.make_random_password())
+        obj.save()
+
+        # This form only requires the "email" field, so will validate.
+        reset_form = PasswordResetForm(self.request.POST)
+        reset_form.is_valid()  # Must trigger validation
+        # Copied from django/contrib/auth/views.py : password_reset
+        opts = {
+            'use_https': self.request.is_secure(),
+            'email_template_name': 'verification.html',
+            'subject_template_name': 'verification_subject.txt',
+            'request': self.request,
+            # 'html_email_template_name': provide an HTML content template if you desire.
+        }
+        # This form sends the email on save()
+        reset_form.save(**opts)
+
+        return redirect('home:signup-done')
 
 def home(request):
     return render(
