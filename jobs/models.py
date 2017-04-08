3ed84c6f6be586706a073ae6666fe3c34ff3bbf0
Jarred Durant <durantjm198@potsdam.edu>
2017-04-08 17:05:39 -0400

    Fixed job model

--- jobs/models.py
+++ jobs/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Job(models.Model):
-    title = models.CharField(default='')
+    title = models.CharField(default='', max_length=140)
     description = models.TextField(default='')
-    zipcode = models.CharField(default='')
-    price = models.DecimalField(max_digits=2)
+    zipcode = models.CharField(default='', max_length=10)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
     # tags: TODO
     due_date = models.DateField()
     # creator: user TODO
