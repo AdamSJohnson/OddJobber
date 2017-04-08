fa300ad864ab4a2101c30f46a729cb93cc5a01f9
GarryG <valepos@gmail.com>
2017-04-08 18:27:33 -0400

    added navbar

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from . views import signup
+from . views import signup, home
 
 urlpatterns = [
     url(r'^signup/', signup, name='signup'),
     url(r'^login/$', auth_views.login, name='login'),
+    url(r'^', home, name='home'),
 ]
