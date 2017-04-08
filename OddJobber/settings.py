815e2e22b4ca36477d5e263d89631b991444400d
Jarred Durant <durantjm198@potsdam.edu>
2017-04-08 19:21:54 -0400

    Incomplete but not broken, sorry Gregg yr navigation bar is fucked

--- OddJobber/settings.py
+++ OddJobber/settings.py
@@ -67,9 +67,11 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
-        'TEMPLATE_CONTEXT_PROCESSORS':
-            ['django.core.context_processors.request']
     },
+]
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'django.core.context_processors.request',
 ]
 
 WSGI_APPLICATION = 'OddJobber.wsgi.application'
