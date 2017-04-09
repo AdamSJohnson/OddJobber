0819c3eaa9c39654c64a7cb987e7221eb2ee496b
GarryG <valepos@gmail.com>
2017-04-08 23:09:03 -0400

    Users can now logout

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from . views import signup, home, myprofile
+from . views import signup, home, myprofile, logout_view
 
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^signup/', signup.as_view(), name='signup'),
 
     url(r'^profile/', myprofile.as_view(), name='profile'),
+
+    url(r'^logout/', logout_view, name='logout'),
 
     url(r'^', home, name='home'),
 ]
