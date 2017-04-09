08f524a23b203eea154400b6ec988b4a4f461abb
Jarred Durant <durantjm198@potsdam.edu>
2017-04-09 08:47:35 -0400

    table looks a lil better

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from .models import Job
+import datetime
 from . import forms
 
 class create_job(CreateView):
@@ -13,6 +14,10 @@ def search_jobs(request):
         bool_tokens = ['jobtitle', 'description', 'zipcode', 'price',
                       'tags', 'due_date']
         jobs_list = Job.objects.all()
+        minDate = datetime.date(1, 1, 1)
+        maxDate = datetime.date(9999, 12, 31)
+        minPrice = 0
+        maxPrice = 200
         for fieldval in request.POST:
             if fieldval in bool_tokens:
               newlist = jobs_list.filter(**{fieldval: request.POST.get(fieldval)})
@@ -22,10 +27,15 @@ def search_jobs(request):
                 minDate = request.POST.get('due_date_low')
         if 'due_date_high' in request.POST:
                 maxDate = request.POST.get('due_date_high')
-        if minDate is not None and maxDate is not None:
-            newlist = jobs_list.filter(due_date__range= [minDate, maxDate])
-            if newlist:
-                jobs_list = newlist
+        newlist = jobs_list.filter(due_date__range= [minDate, maxDate])
+        if newlist:
+            jobs_list = newlist
+
+        if 'price_low' in request.POST:
+            minPrice = request.POST.get('price_low')
+        newlist = jobs_list.filter(price__range=[minPrice, maxPrice])
+        if newlist:
+           jobs_list = newlist
         return render(request, 'list_jobs.html', {'jobs': jobs_list})
     return render(request, 'search_jobs.html', {'jobs': None})
 
